28820c7b3005642b0f769a02c8d3d2ed74b50de4
nabuto0 <77943167+nabuto0@users.noreply.github.com>
2021-02-09 16:18:07 -0400

    Add files via upload

---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const nabutomenu = (prefix, pushname) => {
+    return `◪ *Comandos do ninem*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabutomenu
